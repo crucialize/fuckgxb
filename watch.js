bba7930f5c9d7ed96d9e494c05f6efbe790a7481
crucialize <crucialize@126.com>
2018-08-24 17:15:18 +0800

    .innerHTML=>.appendChild

--- watch.js
+++ watch.js
@@ -1,12 +1,11 @@
 /**
- * 坑1：想用JQuery?先在manifest.content_scripts.js中引入
- * 坑2：操作元素会有隐藏bug（disable video.pause），
- * 所以只能附加到html里
+ * 坑1：想用JQuery?先在manifest.content_scripts.js中引入（建议别用，看坑4）
+ * 坑2：官方文档：“不能访问web页面或其它content script中定义的函数和变量”
  * 坑3：script只能用.appendChild才能生效
- * 
+ * 坑4：所有的对document的改变只能通过appendChild。innerHTML+=会有很多奇怪的错误。
  */
 
- var scriptElement=document.createElement('script')
+let scriptElement=document.createElement('script')
 
 scriptElement.innerHTML=`
 
@@ -20,13 +19,12 @@ setInterval(function() {
         }
 
         try{
-
-            var video00=document.getElementsByTagName('video')[0];
+            let video00=document.getElementsByTagName('video')[0];
             video00.pause = function () { }//disable video.pause
-            //video00.play()
+            video00.play()
         }catch(e92){}
         
-        var progress=$('.video-percent').text()
+        let progress=$('.video-percent').text()
         $('title').text(progress+"%")
         
     } catch(e82){
@@ -39,25 +37,27 @@ setInterval(function() {
 buttonAdded=true
 
 function func97(){
-    var url00=document.URL
+    let url00=document.URL
     url00=url00.substring(0,url00.indexOf("#"))
-    var strNum00=url00.substring(url00.lastIndexOf('/')+1)
-    var Num00=parseInt(strNum00)
-    var strNum01=(Num00+1).toString()
-    var url01=url00.replace(strNum00,strNum01)
+    let strNum00=url00.substring(url00.lastIndexOf('/')+1)
+    let Num00=parseInt(strNum00)
+    let strNum01=(Num00+1).toString()
+    let url01=url00.replace(strNum00,strNum01)
     window.open(url01)
 }
 
 `
 
-document.getElementsByTagName('body')[0].appendChild(scriptElement)
+document.body.appendChild(scriptElement)
 
 //add button
 
-document.getElementsByTagName('body')[0].innerHTML+=`
+let btnElem=document.createElement('div')
+btnElem.innerHTML=`
 
 <div style="z-index:6324;position:absolute;top:0;left:0;">
 <button onclick="func97()">下一个视频</button>
 </div>
 
 `
+document.body.appendChild(btnElem)
