8fae0fa17dfd4067031a81293a959548f5ce55ec
crucialize <crucialize@126.com>
2018-10-15 09:26:06 +0800

    Optimize ui.

--- popup/my.js
+++ popup/my.js
@@ -1,23 +1,15 @@
+let bg = chrome.extension.getBackgroundPage()
+
 $(document).ready(() => {
 
-    //query and update state
-    setInterval(() => {
-        try {
-            let bg = chrome.extension.getBackgroundPage()
-            let flag = bg.__get_flag()
+    $("#t")[0].checked = bg.__get_flag()
 
-            $('#t').text("关闭已经完成的：" + flag.toString())
-        } catch (e86) { }
-    }, 200)
+    $("#t").change(function () {
+        bg.__set_flag(this.checked)
+    })
 
-    //button: toggle flag
-    $('#t').click(() => {
-        let bg = chrome.extension.getBackgroundPage()
-        let flag = bg.__get_flag()
-        if (flag == true)
-            bg.__set_flag(false)
-        else
-            bg.__set_flag(true)
+    $("#I").click(function () {
+        window.open("https://github.com/crucialize/fuckgxb")
     })
 
 })
