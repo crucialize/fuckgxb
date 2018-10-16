d1397e2d99284870f8f6c721a8354b313139ff15
crucialize <crucialize@126.com>
2018-10-16 21:44:27 +0800

    move nextCource to popup,

--- popup/my.js
+++ popup/my.js
@@ -12,4 +12,39 @@ $(document).ready(() => {
         window.open("https://github.com/crucialize/fuckgxb")
     })
 
+    setInterval(() => {
+
+        chrome.tabs.query({ active: true }, function (tab) {
+    
+            var pageUrl = tab[0].url;
+            console.log(pageUrl)
+            if (pageUrl.indexOf("#!state=chapter&chapterId=") > 0) {
+                $("#next").attr("disabled", false);
+            }
+            else {
+                $("#next").attr("disabled", true);
+            }
+        });
+    }, 200)
+    
+    $("#next").click(function () {
+    
+        chrome.tabs.query({ active: true }, function (tab) {
+    
+            var pageUrl = tab[0].url;
+    
+            var r = /\/([0-9]+)#!state=chapter&chapterId=([0-9]+)/
+    
+            var m = pageUrl.match(r)
+    
+            var prvNum = m[1]
+            var p0 = parseInt(prvNum) + 1
+            var newNum = (p0).toString()
+    
+            var newPageUrl = pageUrl.replace(new RegExp(prvNum, "g"), newNum)
+    
+            window.open(newPageUrl)
+        });
+    })
+
 })
